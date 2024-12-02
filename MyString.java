b4894a16bd1dbc243f26d834fba09bd35f10bb1e
guy kalati <guykalati10@gmail.com>
2024-12-02 17:07:52 +0200

    seven

--- MyString.java
+++ MyString.java
@@ -18,13 +18,51 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String finalStr = "";
+        char c = '1';
+        for(int i = 0 ; i < str.length() ; i++){
+            if((str.charAt(i)>=65)&&(str.charAt(i)<=90)){
+                c = (char) (str.charAt(i) + 32);
+                finalStr += c;
+            }
+            else{
+                finalStr += str.charAt(i);
+            }
+
+        }
+        return finalStr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        boolean check = false;
+        if(str2.length()>str1.length()){
+            return false;
+        }
+        
+        for(int i = 0 ; i <= str1.length() - str2.length() ; i++){
+            if (str1.charAt(i) == str2.charAt(0)) {
+                 int count = i+1;
+                for(int j = 1; j < str2.length() ; j++){
+                    if(str1.charAt(count)==str2.charAt(j)){
+                        check = true; 
+                        count++;   
+                    }
+                    else{
+                        check = false;
+                        count++;
+                        break;
+                    }
+
+                    
+                }
+                
+                
+            }
+            if(check==true){
+                return true;
+            }
+        }
         return false;
     }
 }
